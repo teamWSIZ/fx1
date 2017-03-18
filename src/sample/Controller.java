9fb3fb3d6ffcb54186768ef9a0846a651cacb1e4
pmarecki <pmarecki.pro@gmail.com>
2017-03-18 07:47:49 +0100

    init

---
+++ src/sample/Controller.java
@@ -0,0 +1,13 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Controller {
+    @FXML
+    Button myButton;
+
+    public void foo() {
+        System.out.println("Good");
+    }
+}
